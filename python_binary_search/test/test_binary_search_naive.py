cb478f65d1ac4f738206d45945eaffd5571a054c
David Branner <databranner@gmail.com>
2013-10-25 11:18:37 -0400

    add tests for min, max, empty tree, keys includes 0

--- python_binary_search/test/test_binary_search_naive.py
+++ python_binary_search/test/test_binary_search_naive.py
@@ -5,8 +5,34 @@ import sys
 sys.path.append(os.path.join('..'))
 import binary_search_naive as B
 
+def get_random_list():
+    return [random.randint(0, 100000) for i in range(1000)]
+
 def test_insertion():
     for trial in range(10):
-        random_integers = [random.randint(0, 100000) for i in range(1000)]
-        tree = B.populate_tree(random_integers)
-        assert B.inorder_traverse(tree) == sorted(random_integers)
+        random_list = get_random_list()
+        tree = B.populate_tree(random_list)
+        assert B.inorder_traverse(tree) == sorted(random_list)
+
+def test_insertion_including_0():
+    random_list = get_random_list()
+    random_list.append(0)
+    tree = B.populate_tree(random_list)
+    assert B.inorder_traverse(tree) == sorted(random_list)
+
+
+def test_insert_empty():
+    tree = B.populate_tree()
+    assert tree == None
+
+def test_inorder_empty():
+    tree = B.populate_tree()
+    assert B.inorder_traverse(tree) == []
+
+def test_max():
+    random_list = get_random_list()
+    assert B.max(B.populate_tree(random_list)) == max(random_list)
+
+def test_min():
+    random_list = get_random_list()
+    assert B.min(B.populate_tree(random_list)) == min(random_list)
