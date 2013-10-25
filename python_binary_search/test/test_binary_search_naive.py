27b24708259f4b4799091975769511eb2d328d55
David Branner <databranner@gmail.com>
2013-10-25 12:42:44 -0400

    rename test to get_random_list_unique

--- python_binary_search/test/test_binary_search_naive.py
+++ python_binary_search/test/test_binary_search_naive.py
@@ -5,29 +5,29 @@ import sys
 sys.path.append(os.path.join('..'))
 import binary_search_naive as B
 
-def get_random_list():
+def get_random_list_unique():
     random_list = [random.randint(0, 100000) for i in range(1000)]
     return list(set(random_list))
 
 def test_insertion():
     for trial in range(10):
-        random_list = get_random_list()
+        random_list = get_random_list_unique()
         tree = B.populate_tree(random_list)
         assert B.inorder_traverse(tree) == sorted(random_list)
 
 def test_insertion_including_0():
-    random_list = get_random_list()
+    random_list = get_random_list_unique()
     random_list.append(0)
     tree = B.populate_tree(random_list)
     assert B.inorder_traverse(tree) == sorted(random_list)
 
 def test_insertion_non_int_keys():
-    random_list = get_random_list()
+    random_list = get_random_list_unique()
     random_list.append(1.3)
     assert pytest.raises(SystemExit, "B.populate_tree(random_list)")
 
 def test_insertion_dup_keys():
-    random_list = get_random_list()
+    random_list = get_random_list_unique()
     random_list.append(random_list[-1])
     assert pytest.raises(SystemExit, "B.populate_tree(random_list)")
 
@@ -40,9 +40,9 @@ def test_inorder_empty():
     assert B.inorder_traverse(tree) == []
 
 def test_max():
-    random_list = get_random_list()
+    random_list = get_random_list_unique()
     assert B.max(B.populate_tree(random_list)) == max(random_list)
 
 def test_min():
-    random_list = get_random_list()
+    random_list = get_random_list_unique()
     assert B.min(B.populate_tree(random_list)) == min(random_list)
