12dcb58c1314ff2ac56788d772b114e72a1a1c10
David Branner <databranner@gmail.com>
2013-12-05 17:09:22 -0500

    add sorting into sets working

--- python_anagrams/anagrams.py
+++ python_anagrams/anagrams.py
@@ -9,17 +9,25 @@ def anagrams(path = 'DATA', filename = 'dict.txt',
         minlength = 4, maxlength = 6):
     # prepare `to_return`: list of lists to be returned; and other variables
     to_return = []
-    sorted_sets = [set()] * (maxlength - minlength + 1)
+    number_sets = maxlength - minlength + 1
+    sorted_sets = [set() for i in range(number_sets)]
+    print(sorted_sets)
     # get data from file
     with open(os.path.join(path, filename)) as f:
         data = f.read()
     data = data.split()
+    data = data[:20]
     # sort into sets, one set per given length of word; list `sets`
     #       (minlength <= length <= maxlength)
     for item in data:
-        if minlength <= len(item) <= maxlength:
+        index = len(item)
+        if minlength <= index <= maxlength:
             # sort into correct set
-            pass
+            print(item, index)
+            print('target set:', sorted_sets[index - minlength])
+            sorted_sets[index - minlength].add(item)
+#            print('added', item, 'to', sorted_sets[index - minlength])
+            print(sorted_sets)
     # for each set in `sets`: 
         # begin while loop until set is empty
         # pop a word and sort its characters; we call this the "target"
