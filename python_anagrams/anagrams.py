7ab68f7b6b84ff9351544730f834b553efa0eed1
David Branner <databranner@gmail.com>
2013-12-05 17:15:17 -0500

    add: while loop and popping working

--- python_anagrams/anagrams.py
+++ python_anagrams/anagrams.py
@@ -1,7 +1,8 @@
 # anagrams.py
 # David Prager Branner
 # 20131205
-""""""
+"""Given a dictionary, output the top 20 most "anagrammable" 4-, 5-, and
+6-letter words."""
 
 import os, sys
 
@@ -10,13 +11,13 @@ def anagrams(path = 'DATA', filename = 'dict.txt',
     # prepare `to_return`: list of lists to be returned; and other variables
     to_return = []
     number_sets = maxlength - minlength + 1
-    sorted_sets = [set() for i in range(number_sets)]
-    print(sorted_sets)
+    sets_by_length = [set() for i in range(number_sets)]
+    print(sets_by_length)
     # get data from file
     with open(os.path.join(path, filename)) as f:
         data = f.read()
     data = data.split()
-    data = data[:20]
+    data = data[:2000]
     # sort into sets, one set per given length of word; list `sets`
     #       (minlength <= length <= maxlength)
     for item in data:
@@ -24,17 +25,20 @@ def anagrams(path = 'DATA', filename = 'dict.txt',
         if minlength <= index <= maxlength:
             # sort into correct set
             print(item, index)
-            print('target set:', sorted_sets[index - minlength])
-            sorted_sets[index - minlength].add(item)
-#            print('added', item, 'to', sorted_sets[index - minlength])
-            print(sorted_sets)
+#            print('target set:', sets_by_length[index - minlength])
+            sets_by_length[index - minlength].add(item)
+    print(sets_by_length) # debug
     # for each set in `sets`: 
+    for one_set in sets_by_length:
         # begin while loop until set is empty
-        # pop a word and sort its characters; we call this the "target"
+        while one_set:
+            word = one_set.pop()
+        # pop a word, delete punctuation, sort its chars; we call this "target"
         # create new list for the target
         # iterate through remaining words and:
             # sort each
             # if it matches target, add it to target's list and pop it from set
         # if target's list is > length 1, add to else abandon
+    print(sets_by_length) # debug
     return to_return
     
