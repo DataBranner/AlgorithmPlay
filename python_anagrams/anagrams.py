fbbc147fe1754b1f537bccedd74bce85ba9d9bf8
David Branner <databranner@gmail.com>
2013-12-05 16:50:29 -0500

    add prep of `sorted_sets`

--- python_anagrams/anagrams.py
+++ python_anagrams/anagrams.py
@@ -7,15 +7,19 @@ import os, sys
 
 def anagrams(path = 'DATA', filename = 'dict.txt', 
         minlength = 4, maxlength = 6):
-    # prepare `to_return`: list of lists to be returned
+    # prepare `to_return`: list of lists to be returned; and other variables
     to_return = []
+    sorted_sets = [set()] * (maxlength - minlength + 1)
     # get data from file
     with open(os.path.join(path, filename)) as f:
         data = f.read()
     data = data.split()
-    print(data[9999:10005])
     # sort into sets, one set per given length of word; list `sets`
     #       (minlength <= length <= maxlength)
+    for item in data:
+        if minlength <= len(item) <= maxlength:
+            # sort into correct set
+            pass
     # for each set in `sets`: 
         # begin while loop until set is empty
         # pop a word and sort its characters; we call this the "target"
