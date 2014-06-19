b58817aa6007d66c2d690eda231d79dabed0b980
David Branner <databranner@gmail.com>
2014-06-19 11:04:19 -0400

    add working but not tested lexer

--- python_substrings_glob/lexer.py
+++ python_substrings_glob/lexer.py
@@ -14,8 +14,9 @@ def lexer(p):
     # deque() is idempotent; no harm in this on recursion.
     p = deque(p)
     lexed = []
-    lexed_set = []
     while p:
+        # There may be more than one char-set, so always reinitialize lexed_set
+        lexed_set = []
         c = p.popleft()
         tag = 'char'
         if c == '\\':
@@ -23,23 +24,25 @@ def lexer(p):
             lexed.append((tag, p.popleft()))
         elif c == '[':
             # Handle character sets.
-            c_set = p.popleft()
-                # Special test of first character in set: neg. charsets, ].
-                tag = 'set'
-                if not lexed_set: 
-                    if c_set in {'^', '!'}:
-                        # Discard character but change tag.
-                        tag = 'negset'
+            first_c_in_set = p.popleft()
+            # Special test of first character in set: neg. charsets, ].
+            tag = 'set'
+            if first_c_in_set in {'^', '!'}:
+                # Discard character but change tag.
+                tag = 'negset'
+            else:
+                # Close-] can only be added if it is first char in set,
+                # the same as any other non-neg. character in that position.
+                lexed_set.append(first_c_in_set)
+            # Characters after first are added until close-] encountered.
+            while p:
+                # Check for closing of set.
+                c_in_set = p.popleft()
+                if c_in_set == ']':
+                    break
                 else:
-                    # Close-] can only be added if it is first char in set,
-                    # the same as any other non-neg. character in that position.
-                    lexed_set.append(c_set)
-                # Characters after first are added until close-] encountered.
-                while p:
-                    c_set = p.popleft()
-                    if c_set == '\\':
-                        pass
-            lexed.append((tag, lexed_set))
+                    lexed_set.append(c_in_set)
+            lexed.append((tag, set(lexed_set)))
         else:
-            lexer.append((tag, c))
-    return p. lexed
+            lexed.append((tag, c))
+    return lexed
