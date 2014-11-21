cf8a8300f5ee1375018825ea1b8cdd9ae5ff04c4
David Branner <databranner@gmail.com>
2014-11-20 20:40:25 -0500

    add python_kmp and test suite

---
+++ python_kmp/python_kmp.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python
+# python_kmp.py
+# David Prager Branner
+# 20141120
+
+"""Implement the Knuth-Morris-Pratt subsequence-matching algorithm."""
+
+def fill_skip_ahead_array(sequence):
+    """Implement COMPUTE-PREFIX-FUNCTION(P) of Cormen et al., Ch. 32."""
+    skip_ahead_array = [0]
+    prefix_length = 0
+    for cursor, element in enumerate(sequence[1:]):
+        while prefix_length and sequence[prefix_length] != element:
+            prefix_length = skip_ahead_array[prefix_length - 1]
+        if sequence[prefix_length] == element:
+            prefix_length += 1
+        skip_ahead_array.append(prefix_length)
+    return skip_ahead_array
+
+def match(sequence, subsequence):
+    """Implement KMP-MATCHER(T, P) of Cormen et al., Ch. 32, as generator."""
+    length = len(subsequence)
+    if not length:
+        return None
+    skip_ahead_array = fill_skip_ahead_array(subsequence)
+    chars_matched = 0
+    for cursor, element in enumerate(sequence):
+        print(chars_matched,)
+        while chars_matched and subsequence[chars_matched] != element:
+            chars_matched = skip_ahead_array[chars_matched - 1]
+        print(chars_matched,)
+        if subsequence[chars_matched] == element:
+            chars_matched += 1
+        print(chars_matched)
+        if chars_matched == length:
+            chars_matched = skip_ahead_array[chars_matched - 1]
+            yield cursor - length + 1
+
---
+++ python_kmp/test/test_python_kmp.py
@@ -0,0 +1,96 @@
+# test_python_kmp.py
+# 20141120
+
+import sys
+sys.path.append('..')
+import python_kmp as KMP
+import random
+import string
+
+# For testing our results we use a different version of the 
+# COMPUTE-PREFIX-FUNCTION for KMP, by Keith Schwarz.
+# http://www.keithschwarz.com/interesting/code/knuth-morris-pratt/KnuthMorrisPratt.python.html
+# Accessed 20141120.
+def failTable(pattern):
+    # Create the resulting table, which for length zero is None.
+    result = [None]
+    # Iterate across the rest of the characters, filling in the values for the
+    # rest of the table.
+    for i in range(0, len(pattern)):
+        # Keep track of the size of the subproblem we're dealing with, which
+        # starts off using the first i characters of the string.
+        j = i
+        while True:
+            # If j hits zero, the recursion says that the resulting value is
+            # zero since we're looking for the LPB of a single-character
+            # string.
+            if j == 0:
+                result.append(0)
+                break
+            # Otherwise, if the character one step after the LPB matches the
+            # next character in the sequence, then we can extend the LPB by one
+            # character to get an LPB for the whole sequence.
+            if pattern[result[j]] == pattern[i]:
+                result.append(result[j] + 1)
+                break
+            # Finally, if neither of these hold, then we need to reduce the
+            # subproblem to the LPB of the LPB.
+            j = result[j]
+    return result
+
+def random_string(length=100, chars=4):
+    inventory = random.sample(string.ascii_letters, chars)
+    return ''.join(random.choice(inventory) for _ in range(length))
+
+def test_fill_skip_ahead_array_01():
+    assert KMP.fill_skip_ahead_array('abcde') == [0, 0, 0, 0, 0]
+
+def test_fill_skip_ahead_array_02():
+    assert KMP.fill_skip_ahead_array('aaaaa') == [0, 1, 2, 3, 4]
+
+def test_fill_skip_ahead_array_03():
+    assert KMP.fill_skip_ahead_array('aaabaaa') == [0, 1, 2, 0, 1, 2, 3]
+
+def test_fill_skip_ahead_array_04():
+    assert KMP.fill_skip_ahead_array(
+            'aaacaaaaac') == [ 0, 1, 2, 0, 1, 2, 3, 3, 3, 4]
+
+def test_fill_skip_ahead_array_05():
+    assert KMP.fill_skip_ahead_array('ababaca') == [0, 0, 1, 2, 3, 0, 1]
+
+def test_fill_skip_ahead_array_06():
+    assert KMP.fill_skip_ahead_array('ababacababab') == [
+            0, 0, 1, 2, 3, 0, 1, 2, 3, 4, 5, 4]
+
+def test_fill_skip_ahead_array_07():
+    for i in range(10):
+        rand_str = random_string()
+        assert KMP.fill_skip_ahead_array(rand_str) == failTable(rand_str)[1:]
+
+def test_fill_skip_ahead_array_08():
+    for i in range(10):
+        rand_str = random_string(10, 7)
+        assert KMP.fill_skip_ahead_array(rand_str) == failTable(rand_str)[1:]
+
+def test_fill_skip_ahead_array_09():
+    for i in range(10):
+        rand_str = random_string(20, 40)
+        assert KMP.fill_skip_ahead_array(rand_str) == failTable(rand_str)[1:]
+
+def test_fill_skip_ahead_array_10():
+    assert KMP.fill_skip_ahead_array('') == [0]
+
+def test_fill_skip_ahead_array_11():
+    assert KMP.fill_skip_ahead_array(['a', 'c']) == [0, 0]
+
+def test_fill_skip_ahead_array_12():
+    assert KMP.fill_skip_ahead_array(['a', 'c', 'a', 'c']) == [0, 0, 1, 2]
+
+def test_match_01():
+    assert list(KMP.match('abcdabcdabceabcd', 'abcd')) == [0, 4, 12]
+
+def test_match_02():
+    assert list(KMP.match('abcdabcdabceabcd', 'abc')) == [0, 4, 8, 12]
+
+def test_match_03():
+    assert list(KMP.match('abcdabcdabceabcd', '')) == []
